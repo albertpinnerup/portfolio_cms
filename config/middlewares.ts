442242c557d9ec550a3340aa2b70d2b6f54d58a9
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-09-30 09:57:53 +0200

    fixed middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,20 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    {
+        name: 'strapi::session',
+        config: {
+            cookie: {
+                secure: true,
+                sameSite: 'None',
+            },
+        },
+    },
+    'strapi::favicon',
+    'strapi::public',
 ];
